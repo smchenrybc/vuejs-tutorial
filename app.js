720eb1a78d54e1097186559000f0111b02884fad
Sean McHenry <sean@browndesign.com>
2018-08-23 12:53:04 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ * app.js
+ */
+
+// set Vue instance
+new Vue({
+  el: "#vue-app",
+  data: {
+    name: "Sean",
+    age: 33,
+    coordsX: 0,
+    coordsY: 0
+  },
+  methods: {
+    addYears: function(inc) {
+      this.age += inc;
+    },
+    minusYears: function(dec) {
+      this.age -= dec;
+    },
+    updateXY: function(event) {
+      // console.log(event);
+
+      this.coordsX = event.offsetX;
+      this.coordsY = event.offsetY;
+    }
+  }
+});
