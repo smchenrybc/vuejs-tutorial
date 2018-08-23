44b1a01c131fde636c195feebf4a49108ffca14c
Sean McHenry <sean@browndesign.com>
2018-08-23 15:31:11 -0400

    Finished #9

--- app.js
+++ app.js
@@ -6,15 +6,26 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Sean",
-    age: "33"
+    age: 33,
+    a: 0,
+    b: 0
   },
   methods: {
-    // addName: function() {
-    //   this.name =
+    // addToA: function() {
+    //   return this.a + this.age;
     // },
-    // addAge: function() {
-    //   this.age =
-    // },
+    // addToB: function() {
+    //   return this.b + this.age;
+    // }
+  },
+  computed: {
+    addToA: function() {
+      console.log("addToA");
+      return this.a + this.age;
+    },
+    addToB: function() {
+      console.log("addToB");
+      return this.b + this.age;
+    }
   }
 });
