70ab2f66e00c46fc1d38eafb28ea55fa12763878
Sean McHenry <sean@browndesign.com>
2018-08-24 11:57:40 -0400

    Finished #11

--- app.js
+++ app.js
@@ -6,33 +6,9 @@
 new Vue({
   el: "#vue-app",
   data: {
-    available: false,
-    nearby: false
+    error: false,
+    success: false
   },
-  methods: {
-    toggleAvailable: function(available) {
-      if (this.available) {
-        this.available = false;
-      } else {
-        this.available = true;
-      }
-      this.nearby = false;
-    },
-    toggleNearby: function(nearby) {
-      if (this.nearby) {
-        this.nearby = false;
-      } else {
-        this.nearby = true;
-      }
-      this.available = false;
-    }
-  },
-  computed: {
-    compClasses: function() {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
-  }
+  methods: {},
+  computed: {}
 });
