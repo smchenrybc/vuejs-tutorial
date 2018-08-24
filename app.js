e49209c147b4d0e151e79e0c3275eb333dc36659
Sean McHenry <sean@browndesign.com>
2018-08-24 13:40:22 -0400

    Finished #12

--- app.js
+++ app.js
@@ -6,9 +6,21 @@
 new Vue({
   el: "#vue-app",
   data: {
-    error: false,
-    success: false
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+      { name: 'Ryu', age: 27 },
+      { name: 'Akuma', age: 31 },
+      { name: 'Ken', age: 25 }
+    ],
+    dudes: [
+      { name: 'Chad', age: 25 },
+      { name: 'JT', age: 26 }
+    ]
   },
-  methods: {},
-  computed: {}
+  methods: {
+
+  },
+  computed: {
+
+  }
 });
