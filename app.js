44bd3fa6e4d6d65e050791f7b7c98cbb9a92c297
Sean McHenry <sean@browndesign.com>
2018-08-27 10:57:27 -0400

    #15 done

--- app.js
+++ app.js
@@ -2,39 +2,43 @@
  * app.js
  */
 
+Vue.component('greeting', {
+  template: '<p>Hey there, my name is {{ name }}. <button class="pure-button" v-on:click="changeName">Change name</button></p>',
+  data: function() {
+    return {
+      name: "Yoshi"
+    }
+  },
+  methods: {
+    changeName: function() {
+      this.name = "Mario";
+    }
+  }
+});
+
 // set Vue instance
 var one = new Vue({
   el: "#vue-app-one",
   data: {
-    title: "Vue App One"
+
   },
   methods: {
 
   },
   computed: {
-    greet: function() {
-      return 'Hello from App One!';
-    }
+
   }
 });
 
 var two = new Vue({
   el: "#vue-app-two",
   data: {
-    title: "Vue App Two"
+
   },
   methods: {
-    changeTitle: function(event) {
-      one.title = "App 1 New Title";
-      event.target.disabled = true;
-    }
+
   },
   computed: {
-    greet: function() {
-      return 'Hello from App Two!';
-    }
+
   }
 });
-
-// It even works outside instantiation
-// two.title = "Changed from outside!";
